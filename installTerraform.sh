b7697c493237cc900f30dd81551bcd0671931331
MonetCarriere <MonetCarriere@yahoo.com>
2024-10-29 09:25:11 -0700

    added script to check gnupg

---
+++ installTerraform.sh
@@ -0,0 +1,9 @@
+
+# Check if gnupg is installed
+if which gpg > /dev/null 2>&1; then
+  echo 'gnupg and software-properties-common are already installed'
+else
+  echo 'Installing gnupg and software-properties-common...'
+  sudo apt-get update && sudo apt-get install -y gnupg 
+software-properties-common
+fi
