b93abdb07e68eab0cc74ed3af86770d1e0d19efe
MonetCarriere <MonetCarriere@yahoo.com>
2024-10-31 09:04:28 -0700

    updated the terraform install script to include the cache modification

--- TFInstall.sh
+++ TFInstall.sh
@@ -7,12 +7,13 @@ else
   echo "Terraform is not installed. Proceeding with installation."
 fi
 
-# Check if gnupg is installed
-if (which gpg)
-then
-  echo "gnupg is already installed."
+# Check if gnupg is installed and if software-properties-common is 
+available in the package cache.
+if which gpg && apt-cache show software-properties-common; then
+  echo 'gnupg and software-properties-common are already installed or 
+available'
 else
-  echo "Installing gnupg"
-  sudo apt-get install -y gnupg
+  echo 'Installing gnupg and software-properties-common...'
+  sudo apt-get update && sudo apt-get install -y gnupg 
+software-properties-common
 fi
-
