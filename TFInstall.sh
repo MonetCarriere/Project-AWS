18f86bdfc39b6cf4f78a3748a7f6bd3922456adc
MonetCarriere <MonetCarriere@yahoo.com>
2024-11-04 08:43:41 -0800

    added the software properties script to the tf install file

--- TFInstall.sh
+++ TFInstall.sh
@@ -6,6 +6,7 @@ then
 else
   echo "Terraform is not installed. Proceeding with installation."
 fi
+
 # Check if gnupg is installed
 if (which gpg)
 then
@@ -13,5 +14,13 @@ then
 else
   echo "Installing gnupg"
   sudo apt-get install -y gnupg
+fi
+
+# Check if software-properties-common is installed
+if dpkg -s software-properties-common > /dev/null 2>&1; then
+  echo 'software-properties-common is already installed'
+else
+  echo 'Installing software-properties-common...'
+  sudo apt-get update && sudo apt-get install -y software-properties-common
 fi
 
