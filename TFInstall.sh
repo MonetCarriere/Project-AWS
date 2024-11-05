03b0b5c178724712fd604d058536ffd8b15aed6a
jlotto8 <jlotto8@gmail.com>
2024-11-05 00:55:27 -0500

    Fix merge conflict, and edit script to use gpg command.

--- TFInstall.sh
+++ TFInstall.sh
@@ -2,7 +2,6 @@
 if (which terraform)
 then
   echo "Terraform is already installed."
-  exit 0
 else
   echo "Terraform is not installed. Proceeding with installation."
 fi
@@ -15,6 +14,13 @@ else
   sudo apt-get install -y gnupg
 fi
 
+# Check if software-properties-common is installed
+if dpkg -s software-properties-common > /dev/null 2>&1; then
+  echo 'software-properties-common is already installed'
+else
+  echo 'Installing software-properties-common...'
+  sudo apt-get install -y software-properties-common
+fi
 
 # Check if the HashiCorp GPG key and repository are added
 if apt-cache policy terraform | grep -q 'hashicorp'
