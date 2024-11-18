6bc8df7c3607e1282adbe21f6408f844321aa06b
MonetCarriere <MonetCarriere@yahoo.com>
2024-11-18 09:34:43 -0800

    updated the conditional to include stephenssuggestion

--- terraformInstall.sh
+++ terraformInstall.sh
@@ -9,6 +9,24 @@ else
     echo "Failed to update package lists ❌"
 fi
 
+#Checks if gnupg is in the cache
+if (apt-cache show gnupg)
+then
+  echo "gnupg already exists in the apt cache👍"
+else
+  echo "updating apt cache 🔄 "
+  sudo apt update
+fi
+
+#Checks if software-properties-common is in the cache
+if (apt-cache show software-properties-common)
+then
+  echo "software-properties-common already exists in the apt cache👍"
+else
+  echo "updating apt cache 🔄 "
+  sudo apt update
+fi
+
 # Install gnupg and software-properties-common
 echo "Installing gnupg and software-properties-common..."
 if sudo apt-get install -y gnupg software-properties-common; then
@@ -18,30 +36,30 @@ else
 fi
 
 ## CONDITIONAL 2
-#echo "Adding HashiCorp GPG key..."
-#wget -O- https://apt.releases.hashicorp.com/gpg | \
-   # gpg --dearmor | \
-   # sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+echo "Adding HashiCorp GPG key..."
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
 
 ## CONDITIONAL 3
-#echo "Verifying GPG key..."
-#gpg --no-default-keyring \
-   # --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-   # --fingerprint
+echo "Verifying GPG key..."
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
 
 ## CONDITIONAL 4
-#echo "Adding HashiCorp repository..."
-#echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-   # https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-   # sudo tee /etc/apt/sources.list.d/hashicorp.list
+echo "Adding HashiCorp repository..."
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 ## CONDITIONAL 5
-#echo "Updating package lists..."
-#sudo apt update
+echo "Updating package lists..."
+sudo apt update
 
 ## CONDITIONAL 6
-#echo "Installing Terraform..."
-#sudo apt-get install terraform
+echo "Installing Terraform..."
+sudo apt-get install terraform
 
 ## CONDITIONAL 7
-#echo "Installation complete. Run 'terraform --version' to verify."
+echo "Installation complete. Run 'terraform --version' to verify."
